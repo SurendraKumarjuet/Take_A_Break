9019b3b15fa581e461f68621d610b5ee50f78476
Surendra Kumar <32401892+SurendraKumarjuet@users.noreply.github.com>
2019-01-30 00:15:55 +0530

    Add files via upload

---
+++ Take_A_Break.py
@@ -0,0 +1,11 @@
+import webbrowser
+import time
+import random
+
+a=("https://www.youtube.com/watch?v=U39tdiKiaUQ","https://www.youtube.com/watch?v=mYgFJuiAGrg","https://www.youtube.com/watch?v=byitAI7kkOM")
+i=1
+print("The Break Starts at :"+time.ctime())
+while(i<=3):	
+	time.sleep(10)
+	webbrowser.open(random.choice(a))
+	i +=1
